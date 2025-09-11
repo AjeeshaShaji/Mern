775749603412c266128abe983c3db37bd8e89e6b
AjeeshaShaji <ajeeshashaji2357@gmail.com>
2025-09-11 14:20:24 +0530

    files are updated

--- javascript/arraymethod.js
+++ javascript/arraymethod.js
@@ -1,83 +1,85 @@
 
-// let a=[2,4,6,8,10];
-// a.push(12);
-// console.log(a);
+let a=[2,4,6,8,10];
+a.push(12);
+console.log(a);
 
 
 
-// let b=[1,2,3,4,5,6];
-// b.pop()
-// console.log(b);
+let b=[1,2,3,4,5,6];
+b.pop()
+console.log(b);
 
 
 
-// let c=[9,8,7,6,5,4];
-// c.unshift(10)
-// console.log(c);
+let c=[9,8,7,6,5,4];
+c.unshift(10)
+console.log(c);
 
 
 
-// let d=[9,8,7,6,5,4];
-// d.shift()
-// console.log(d);
+let d=[9,8,7,6,5,4];
+d.shift()
+console.log(d);
 
 
 
-// let e=[2,4,6,8,10,12,14,16,18,20];
-// e.splice(3,2)
-// console.log(e);
+let e=[2,4,6,8,10,12,14,16,18,20];
+e.splice(3,2)
+console.log(e);
 
 
 
-// let f=[2,4,6,8,10,12,14,16,18,20];
-// f.splice(4)
-// console.log(f);
+let f=[2,4,6,8,10,12,14,16,18,20];
+f.splice(4)
+console.log(f);
 
 
 
-// let g=[2,4,6,8,10,12,14,16,18,20];
-// g.splice(3,2,50,60)
-// console.log(g);
+let g=[2,4,6,8,10,12,14,16,18,20];
+g.splice(3,2,50,60)
+console.log(g);
 
 
-// let h=[2,4,6,8,10,12,14,16,18,20];
-// h.splice(3,0,40,70)
-// console.log(h);
+let h=[2,4,6,8,10,12,14,16,18,20];
+h.splice(3,0,40,70)
+console.log(h);
 
 
-// let i=[1,2,3,6,4,5];
-// let j=i.slice(2,5)
-// console.log(j);
+let i=[1,2,3,6,4,5];
+let j=i.slice(2,5)
+console.log(j);
 
 
-// let k=[1,2,3];
-// let l=[4,6,8];
-// let m=k.concat(l)
-// console.log(m);
-
-
-
-// let n=[1,2,3,4,5,6,7,8,9];
-// let out=n.includes(10)
-// console.log(out);
+let k=[1,2,3];
+let l=[4,6,8];
+let m=k.concat(l)
+console.log(m);
 
 
 
-// let o=[1,2,3,4,5,6,7,8,9,10];
-// let p=o.indexOf(6);
-// console.log(p);
+let n=[1,2,3,4,5,6,7,8,9];
+let out=n.includes(10)
+console.log(out);
 
 
 
-// let q=[1,2,3,4,5,6,7,8,9,10];
-// let str=q.join('+');
-// console.log(str);
+let o=[1,2,3,4,5,6,7,8,9,10];
+let p=o.indexOf(6);
+console.log(p);
 
 
 
-// let r=[1,2,3,4,5,6,7,8,9,10];
-// let rev=r.reverse();
-// console.log(rev);
+let q=[1,2,3,4,5,6,7,8,9,10];
+let str=q.join('+');
+console.log(str);
 
 
 
+let r=[1,2,3,4,5,6,7,8,9,10];
+let rev=r.reverse();
+console.log(rev);
+
+
+ let x=[1,32,3,430,15,65,7,98,9,10];
+ let sorted=x.sort((a,b)=>b-a);
+ console.log(sorted);
---
+++ javascript/arrfn.js
@@ -0,0 +1,7 @@
+
+
+let bet=[1,2,3,4,5,6];
+let aa=bet.filter((item,index)=>{
+    return item%2==0;
+})
+console.log(aa);
